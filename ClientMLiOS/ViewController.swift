366f402247b8281cc33e7450404af2583a636d3a
Vidya Murthy <vidya.murthy@scout24.com>
2018-08-27 16:26:05 +0200

    Initial commit

---
+++ ClientMLiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClientMLiOS
+//
+//  Created by Vidya Murthy on 27.08.18.
+//  Copyright © 2018 Vidya Murthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
