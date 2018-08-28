ca993257f373c47917b4771d22425e0682769cf1
Serban Florin <serban.popa@scout24.com>
2018-08-28 10:20:47 +0200

    created Button bag

---
+++ ClientMLiOS/ButtonBag.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ButtonBag: UIStackView {
+    public func setButtons(_ buttonTitles: [String]) {
+        
+    }
+    
+    public func selectedButtons() -> [String]? {
+        return nil
+    }
+    
+    public func resetSelected() {
+        
+    }
+}
--- ClientMLiOS/ViewController.swift
+++ ClientMLiOS/ViewController.swift
@@ -1,25 +1,15 @@
-//
-//  ViewController.swift
-//  ClientMLiOS
-//
-//  Created by Vidya Murthy on 27.08.18.
-//  Copyright © 2018 Vidya Murthy. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var statsHUD: StatsView!
+    @IBOutlet weak var buttonBag: ButtonBag!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func resetAction(_ sender: Any) {
     }
-
-
 }
 
