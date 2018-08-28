d445a83428845b86d4fbd364a523749165d6c8bd
Serban Florin <serban.popa@scout24.com>
2018-08-28 11:19:44 +0200

    implement ui updating

---
+++ ClientMLiOS/ScoreCalculator.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class ScoreCalculator {
+    public func scoreFor(_ tags: [String]) -> MLScoreTuple {
+        return (false, 0)
+    }
+}
--- ClientMLiOS/ViewController.swift
+++ ClientMLiOS/ViewController.swift
@@ -1,15 +1,27 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ButtonBagDelegate {
     
     @IBOutlet weak var statsHUD: StatsView!
     @IBOutlet weak var buttonBag: ButtonBag!
+    private let scoreCalculator = ScoreCalculator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonBag.delegate = self
     }
     
     @IBAction func resetAction(_ sender: Any) {
+        buttonBag.resetSelected()
+        let score = scoreCalculator.scoreFor([String]())
+        statsHUD.updateWithScore(score)
+    }
+    
+    func buttonBagDidUpdate() {
+        if let selectedTags = buttonBag.selectedButtons() {
+            let score = scoreCalculator.scoreFor(selectedTags)
+            statsHUD.updateWithScore(score)
+        }
     }
 }
 
