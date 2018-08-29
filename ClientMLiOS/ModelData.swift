4b0c54cf31d2dbf66adc0ac2fd4221e246a41df0
Serban Florin <serban.popa@scout24.com>
2018-08-29 12:01:22 +0200

    added model and model loading

--- ClientMLiOS/ModelData.swift
+++ ClientMLiOS/ModelData.swift
@@ -1,12 +1,27 @@
-class ModelData {
+import Foundation
+
+class ModelData: Codable {
+    let weights: [[Double]]?
+    let vectors: [[Double]]?
+    let bias: Double?
     
-    let modelBias = 0.0
-    let modelWeights = [[Double]]()
-    let modelVectors = [[Double]]()
-    
-    public func loadFromFile() {
-        //TODO: load data into arrays from jsons
+    enum CodingKeys: String, CodingKey {
+        case weights = "weights"
+        case vectors = "vectors"
+        case bias = "bias"
+    }
+
+    static public func loadFromFile() -> ModelData {
+        let fileURL = URL(fileURLWithPath: Bundle.main.path(forResource: "model", ofType: "json")!)
+        let jsonData = try! Data(contentsOf: fileURL)
+        let model = try! JSONDecoder().decode(ModelData.self, from: jsonData)
+        
+        return model
     }
     
-    
+    init(weights: [[Double]]?, vectors: [[Double]]?, bias: Double?) {
+        self.weights = weights
+        self.vectors = vectors
+        self.bias = bias
+    }
 }
